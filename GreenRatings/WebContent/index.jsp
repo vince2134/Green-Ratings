6969aad36031a8367268eb15810a3d027c5fec48
dhirentheindian <dhirendhanani@gmail.com>
2016-08-03 19:18:17 +0800

    LoginServlet w/ working username and pw checker

--- GreenRatings/WebContent/index.jsp
+++ GreenRatings/WebContent/index.jsp
@@ -159,14 +159,14 @@
         <center><div class="main card">
             <div id="sprite_header"></div><br>
             <div class="login_field">
-                <form class="reg_form_card">
+                <form class="reg_form_card" action="LoginServlet" method = "post">
                <div id = "caption">Sign in to see ratings of your favorite professors in DLSU.</div>
                     <div>
-                        <input type="text" class="username drtz" aria-required="true" placeholder="Username">
+                        <input type="text" class="username drtz" aria-required="true" placeholder="Username" name ="username">
                     </div>
                     
                     <div>
-                        <input type="password" class="password drtz" aria-required="true" placeholder="Password">
+                        <input type="password" class="password drtz" aria-required="true" placeholder="Password" name = "password">
                     </div>
                     <div>
                         <button id="login_btn" class="login btn" style="vertical-align:middle"><span><a href="user_home.html">Log in </a></span></button>
--- GreenRatings/src/edu/webapde/servlet/Welcome.java
+++ GreenRatings/src/edu/webapde/servlet/Welcome.java
@@ -7,46 +7,38 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import edu.webapde.object.User;
-import edu.webapde.services.UserService;
-
 /**
- * Servlet implementation class RegisterServlet
+ * Servlet implementation class Welcome
  */
-@WebServlet("/RegisterServlet")
-public class RegisterServlet extends HttpServlet {
+@WebServlet("/WelcomeJ")
+public class Welcome extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public RegisterServlet() {
+    public Welcome() {
         super();
-
+        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-		request.getRequestDispatcher("register.jsp").forward(request, response);
+
+		request.getRequestDispatcher("index.jsp").forward(request, response);
+		
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		User u = new User();
-		u.setUsername(request.getParameter(User.COLUMN_UN));
-		u.setEmail(request.getParameter(User.COLUMN_EMAIL));
-		u.setPassword(request.getParameter(User.COLUMN_PW));
-		
-		//tell the service to add the new object
-		UserService userService = new UserService();
-		userService.addUser(u);
-		request.getRequestDispatcher("index.jsp").forward(request, response);
-
+		// TODO Auto-generated method stub
+		doGet(request, response);
 	}
 
 }
